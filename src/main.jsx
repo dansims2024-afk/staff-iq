8bb7aed028becd6eaf923ae075082e46eb871061
dansims2024-afk <dansims.2024@gmail.com>
2026-01-06 14:52:04 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // <--- CHECK THIS NAME CAREFULLY
-import './index.css'
+import App from './App.jsx' 
+
+// If index.css is missing or has a different name, 
+// removing or commenting out the line below will fix the build error.
+// import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
