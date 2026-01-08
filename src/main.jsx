0d6b1f3d9a4641fda7961273ebbf233fcf60f645
dansims2024-afk <dansims.2024@gmail.com>
2026-01-08 08:50:59 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx' // <--- This forces it to look at your new file
+import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
